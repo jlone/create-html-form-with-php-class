228d83178a30abd0a545d7d33fd8b6ea24c82f43
jlone <jlone@jlone.com>
2014-05-29 09:06:16 -0400

    add form_input id's to form.php

--- form.php
+++ form.php
@@ -7,25 +7,25 @@
 
     echo $form->form_open('POST');
     echo $form->form_label('Full Name');
-    echo $form->form_input('text', 'name', 'Please Type your name');
+    echo $form->form_input('text', 'name', 'name', 'Please Type your name');
 
     echo '<br/>';
 
     echo $form->form_label('Email');
-    echo $form->form_input('email', 'email', 'Please Type your valid email');
+    echo $form->form_input('email', 'email', 'email', 'Please Type your valid email');
 
 
     echo '<br/>';
 
     echo $form->form_label('Password');
-    echo $form->form_input('password', 'password', 'Please write your password');
+    echo $form->form_input('password', 'password', 'password', 'Please write your password');
 
 
 
     echo '<br/>';
 
     echo $form->form_label('Address');
-    echo $form->form_textarea('address', 'Please write your address');
+    echo $form->form_textarea('address', 'address', Please write your address');
 
 
 
